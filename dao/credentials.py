9ed1f497ee75f30d6eb3141248f2ffc7b5b9931a
Zlata Khodos <zlata_khodos@ukr.net>
2019-12-14 17:22:26 +0200

    credentials changed

--- dao/credentials.py
+++ dao/credentials.py
@@ -1,9 +1,9 @@
 import os
 
-username = 'fmasjvreysufdc'
-password = '412d682250ed844e5870ac10bef7d4e1df78cb0a5a22c20b9e396b496c51efb0'
-host = 'ec2-107-21-108-37.compute-1.amazonaws.com'
+username = 'itztxiljkesfln'
+password = '96aa77c3f8d47eded34a0d51daa1078844af3440ff274c894501cfe2901b6898'
+host = 'ec2-174-129-33-132.compute-1.amazonaws.com'
 port = '5432'
-database = 'daocv3uoo1ckhc'
+database = 'deqc8q6p8enj73'
 DATABASE_URI = os.getenv("DATABASE_URL",
                          'postgres://itztxiljkesfln:96aa77c3f8d47eded34a0d51daa1078844af3440ff274c894501cfe2901b6898@ec2-174-129-33-132.compute-1.amazonaws.com:5432/deqc8q6p8enj73')
