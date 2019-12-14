5043217f0db1fda6347414cb2d3ea9f107ddaa05
Zlata Khodos <zlata_khodos@ukr.net>
2019-12-14 16:49:32 +0200

    credentials new

--- dao/credentials.py
+++ dao/credentials.py
@@ -1,9 +1,9 @@
 import os
 
-username = 'postgres'
-password = 'postgres'
-host = 'localhost'
+username = 'fmasjvreysufdc'
+password = '412d682250ed844e5870ac10bef7d4e1df78cb0a5a22c20b9e396b496c51efb0'
+host = 'ec2-107-21-108-37.compute-1.amazonaws.com'
 port = '5432'
-database = 'postgres'
+database = 'daocv3uoo1ckhc'
 DATABASE_URI = os.getenv("DATABASE_URL",
-                         'postgres+psycopg2://postgres:{}@{}:{}/{}'.format(password, host, port, database))
+                         'postgres://fmasjvreysufdc:412d682250ed844e5870ac10bef7d4e1df78cb0a5a22c20b9e396b496c51efb0@ec2-107-21-108-37.compute-1.amazonaws.com:5432/daocv3uoo1ckhc')
